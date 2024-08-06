576305a9b9683029cf9b607b7dd809026bc0b9e1
Mikael <mikael@axblom.se>
2024-08-06 10:14:54 +0200

    Remove old command code

--- localsJsonDumper/DumpObjectCommandPackage.cs
+++ localsJsonDumper/DumpObjectCommandPackage.cs
@@ -61,9 +61,8 @@ namespace LocalsJsonDumper
         {
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
-            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-            //await DumpObjectCommand.InitializeAsync(this);
-            await LocalsJsonDumper.ExportToolWindowCommand.InitializeAsync(this);
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);            
+            await ExportToolWindowCommand.InitializeAsync(this);
         }
 
         #endregion Package Members
